86723b11071931009a7879eee53c95343cc22243
naitikattri2410-oss <naitikattri2410@gmail.com>
2026-05-02 14:21:54 +0530

    first commit

---
+++ zerocross.py
@@ -0,0 +1,64 @@
+def print_board(tic):
+    print("your game")
+    for row in tic:
+        print(" ".join(row))
+    print()
+
+
+def check_winner(tic, c):
+    return (
+        (tic[0][0] == tic[1][1] == tic[2][2] != '_') or
+        (tic[0][0] == tic[0][1] == tic[0][2] != '_') or
+        (tic[1][0] == tic[1][1] == tic[1][2] != '_') or
+        (tic[2][0] == tic[2][1] == tic[2][2] != '_') or
+        (tic[0][0] == tic[1][0] == tic[2][0] != '_') or
+        (tic[0][1] == tic[1][1] == tic[2][1] != '_') or
+        (tic[0][2] == tic[1][2] == tic[2][2] != '_') or
+        (tic[0][2] == tic[1][1] == tic[2][0] != '_')
+    )
+
+
+def is_draw(tic):
+    for row in tic:
+        if '_' in row:
+            return False
+    return True
+
+
+def main():
+    tic = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]
+    print("game will start with X")
+    c = 'X'
+
+    while True:
+        try:
+            a = int(input("row: "))
+            b = int(input("column: "))
+        except ValueError:
+            print("invalid input. try again.")
+            continue
+
+        # check weather thi input is valid
+        if a > 3 or a < 1 or b > 3 or b < 1:
+            print("invalid place.\n")
+            continue
+
+        if tic[a - 1][b - 1] != '_':
+            print("not available..try again\n")
+            continue
+
+        tic[a - 1][b - 1] = c
+        print_board(tic)
+
+        # for winner 
+        if check_winner(tic, c):
+            print(f"{c} is winner")
+            break
+
+        # used to check if the game is draw
+        if is_draw(tic):
+            print("match is draw")
+            break
+
+        c = 'O' if c == 'X' else 'X'
+main()
